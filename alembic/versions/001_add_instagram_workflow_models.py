c3b241f222e2ea9620d1fec08376969db0eff63c
alies707 <mignamasmali96@gmail.com>
2026-08-31 19:13:16 +0330

    Add initial Instagram workflow migration placeholder

---
+++ alembic/versions/001_add_instagram_workflow_models.py
@@ -0,0 +1,17 @@
+"""add instagram workflow models
+
+Revision ID: 001_add_instagram_workflow_models
+"""
+
+from alembic import op
+
+revision = "001_add_instagram_workflow_models"
+down_revision = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
